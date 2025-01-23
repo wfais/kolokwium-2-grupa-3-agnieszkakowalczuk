38239c444bd63278037e9621a2b1606a3766e00a
Agnieszka Kowalczuk <agamaria05@gmail.com>
2025-01-23 08:28:10 +0100

    Update main.py

--- ZADANIE3/main.py
+++ ZADANIE3/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+from sympy.physics.control.control_plots import matplotlib
+
 
 # Wygeneruj dane (np. 2 tablice x i y o długości 10) za pomocą NumPy.
 # Narysuj wykres punktowy (funkcją ax.scatter(...) lub plt.scatter(...)).
@@ -10,6 +13,7 @@ import matplotlib.pyplot as plt
 def draw_scatter_plot():
     """
     1) Wygeneruj dane x, y (np. np.arange(10) i jakieś y losowe).
+
     2) Rysuj wykres punktowy (scatter).
     3) Ustaw etykiety osi, tytuł, legendę.
     4) Zwróć obiekt Figure.
@@ -17,17 +21,22 @@ def draw_scatter_plot():
     np.random.seed(123)
     
     # utwórz x, y
-    # x = ...
-    # y = ...
-    
-    # fig, ax = 
-    
-    # ax.scatter( ... )
+    x = np.arange(10)
+    y = np.random.rand(10)
+
+    fig, ax = plt.subplots()
+    ax.scatter(x,y) # rysuj wykres punktowy
     
     # label dla x, y, title, legend
+
+    plt.xlabel("X axis")
+    plt.ylabel("Y axis")
+    plt.title("Scatter plot")
+    plt.legend(["Data"])
+
     
     # return rysunek
-    pass
+    return fig
 
 if __name__ == '__main__':
     fig = draw_scatter_plot()
