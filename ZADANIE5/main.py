8b91726c8b4faa44133c0ef06a01e4d574ec5912
Agnieszka Kowalczuk <agamaria05@gmail.com>
2025-01-23 09:03:51 +0100

    Update main.py

--- ZADANIE5/main.py
+++ ZADANIE5/main.py
@@ -14,26 +14,36 @@ def create_app():
     2) Pole Entry
     3) Przycisk "Pokaż", który wyświetli wpisany tekst w innej etykiecie
     """
-    # title - "Prosta aplikacja Tkinter"
+    #title - "Prosta aplikacja Tkinter"
+    root = tk.Tk()
+    root.title("Prosta aplikacja Tkinter")
 
     # label_instruct = umocuj przez pack
+    label_instruct = tk.Label(root, text="Wpisz coś:")
+    label_instruct.pack()
 
-    # entry_text = 
+    # entry_text =
+    entry_text = tk.Entry(root)
+    entry_text.pack()
 
-    # label_result = tk.Label(...
+    label_result = tk.Label(root, text="")
+    label_result.pack()
+
 
     # zdefiniuj funkcję show_text() pobierającą wpisany tekst i wyświetlającą w label_result
-    # def show_text():
-    #     ...
-    #     label_result.config(...)
+    def show_text():
+        text = entry_text.get()
+        label_result.config(text=f"Wpisałeś: {text}")
 
-    # button_show = 
-    # button_show.pack()
+    button_show = tk.Button(root, text="Pokaż", command=show_text)
+    button_show.pack()
 
     # TODO: return root
-    pass
+    return root
 
 if __name__ == '__main__':
     # TODO: app = create_app()
+    app=create_app()
     # TODO: app.mainloop()
+    app.mainloop()
     pass
